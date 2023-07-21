64b670b2ee9138cb29138dec7e092f577f02afc2
thanhnguyen1502 <daupkaiemckuatung@gmail.com>
2023-07-21 08:44:10 +0700

    update

--- BE/tokei/src/main/java/com/example/tokei/repository/product/IProductRepository.java
+++ BE/tokei/src/main/java/com/example/tokei/repository/product/IProductRepository.java
@@ -1,8 +1,6 @@
 package com.example.tokei.repository.product;
 
 import com.example.tokei.model.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
